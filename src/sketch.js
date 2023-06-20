f3e36f0c3738c6b07bd380decd310eabe790ff78
SaraPrada <prada_sara@libero.it>
2023-06-20 21:16:30 +0200

    Aggiustamenti saturazione colori

--- src/sketch.js
+++ src/sketch.js
@@ -71,10 +71,10 @@ async function draw() {
         ampB = map(indiceB.y, windowHeight  / 2.15, 0, 0, 1); // Mappa la coordinata Y dell'indice B sull'ampiezza
 
         // Imposta il colore di riempimento con la sfumatura di blu
-        let hue = map(indiceA.x, 0, width, 180, 255); // Sfumature di blu da 180 a 255
-        let saturation = map(indiceA.y, 0, height, 100, 0); // Graduazione della saturazione
-        let hueB = map(indiceB.x, 0, width, 20, 95); // Sfumature di blu da 180 a 255
-        let saturationB = map(indiceB.y, 0, height, 100, 0); // Graduazione della saturazione
+        let hue = map(indiceA.x, 0, windowWidth / 1.2, 180, 255); // Sfumature di blu da 180 a 255
+        let saturation = map(indiceA.y, 0, windowHeight / 1.2, 100, 0); // Graduazione della saturazione
+        let hueB = map(indiceB.x, 0, windowWidth / 1.2, 20, 95); // Sfumature di blu da 180 a 255
+        let saturationB = map(indiceB.y, 0, windowHeight / 1.2, 100, 0); // Graduazione della saturazione
         let brightness = 100; // Luminosità massima
         let offset = 40;
 
