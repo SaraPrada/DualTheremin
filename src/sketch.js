38b0a61443fafbac482a550224112ef87c7576a1
SaraPrada <prada_sara@libero.it>
2023-06-20 19:00:18 +0200

    Amplificatore a 0 nella parte bassa dello schermo

--- src/sketch.js
+++ src/sketch.js
@@ -8,6 +8,9 @@ let playingA = false, playingB = false; // Indicatori per il suono di oscA e osc
 let freqA, ampA, freqB, ampB; // Frequenza e ampiezza per oscA e oscB
 
 
+
+
+
 async function setup() {
   createCanvas(windowWidth, windowHeight);
   oscA = new p5.Oscillator('sine');
@@ -61,11 +64,11 @@ async function draw() {
           isSquareSound = true; // Indice associato ai quadrati (suono a onda quadrata)
         }
 
-        freqA = map(indiceA.x, 0, windowWidth, 100, 500); // Mappa la coordinata X dell'indice A sulla frequenza
-        ampA = map(indiceA.y, windowHeight, 0, 0, 1); // Mappa la coordinata Y dell'indice A sull'ampiezza
+        freqA = map(indiceA.x, 0, windowWidth, 100, 600); // Mappa la coordinata X dell'indice A sulla frequenza
+        ampA = map(indiceA.y, windowHeight  / 2, 0, 0, 1); // Mappa la coordinata Y dell'indice A sull'ampiezza
 
-        freqB = map(indiceB.x, 0, windowWidth, 100, 500); // Mappa la coordinata X dell'indice B sulla frequenza
-        ampB = map(indiceB.y, windowHeight, 0, 0, 1); // Mappa la coordinata Y dell'indice B sull'ampiezza
+        freqB = map(indiceB.x, 0, windowWidth, 100, 800); // Mappa la coordinata X dell'indice B sulla frequenza
+        ampB = map(indiceB.y, windowHeight  / 2, 0, 0, 1); // Mappa la coordinata Y dell'indice B sull'ampiezza
 
         // Imposta il colore di riempimento con la sfumatura di blu
         let hue = map(indiceA.x, 0, width, 180, 255); // Sfumature di blu da 180 a 255
